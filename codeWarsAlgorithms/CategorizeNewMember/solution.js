719dda7d96c7ef46571299a620aa4103129ec9b5
MintMissy <dawidkostka480@gmail.com>
2022-02-02 21:52:20 +0100

    Codewars - Upgrade New Member Algorithm

--- codeWarsAlgorithms/CategorizeNewMember/solution.js
+++ codeWarsAlgorithms/CategorizeNewMember/solution.js
@@ -1,12 +1,18 @@
-function openOrSenior(data) {
-  let resultList = [];
+// function openOrSenior(data) {
+//   let resultList = [];
 
-  for (const dataItem of data) {
-    if (dataItem[0] >= 55 && dataItem[1] > 7) {
-      resultList.push('Senior');
-    } else {
-      resultList.push('Open');
-    }
-  }
-  return resultList;
+//   for (const dataItem of data) {
+//     if (dataItem[0] >= 55 && dataItem[1] > 7) {
+//       resultList.push('Senior');
+//     } else {
+//       resultList.push('Open');
+//     }
+//   }
+//   return resultList;
+// }
+
+function openOrSenior(data) {
+  return data.map(([age, handicap]) =>
+    age >= 55 && handicap > 7 ? 'Senior' : 'Open'
+  );
 }
