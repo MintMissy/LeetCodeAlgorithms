d405f1c7f0377104e75b894a88415f2d1199b3fc
MintMissy <dawidkostka480@gmail.com>
2022-02-03 17:19:07 +0100

    Codewars - Is my friend cheating algorithm

---
+++ codeWarsAlgorithms/LogicThinking/isMyFriendCheating.js
@@ -0,0 +1,35 @@
+// function removeNb(n) {
+//   const results = [];
+//   let sum = (n * (n + 1)) / 2;
+
+//   for (let i = 1; i <= n; i++) {
+//     for (let j = 1; j <= n; j++) {
+//       if (sum - i - j === i * j) {
+//         results.push([i, j]);
+//       }
+//     }
+//   }
+
+//   return results;
+// }
+
+function removeNb(n) {
+  const results = [];
+  let sum = (n * (n + 1)) / 2;
+  console.log(sum);
+
+  for (let x = 1; x <= n; x++) {
+    let y = (sum - x) / (x + 1);
+    console.log(x);
+    console.log(y);
+    if (y <= n && x * y === sum - x - y) {
+      if ((x + y) % 1 === 0) {
+        results.push([x, y]);
+      }
+    }
+  }
+
+  return results;
+}
+
+console.log(removeNb(26));
